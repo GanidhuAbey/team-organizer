e6dfbb30d175329f123356c4fdd27c8822ec03b4
GanidhuAbey <ganidhu.abey@gmail.com>
2022-01-06 13:02:07 -0700

    removed unused import

--- employees/views.py
+++ employees/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from django.views.generic import ListView
 from django.urls import reverse
-import phonenumbers
 
 
 #import model data
